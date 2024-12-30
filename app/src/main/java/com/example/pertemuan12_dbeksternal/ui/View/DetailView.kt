de45efa61698c3571d96f94b5630723a0b2984de
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-30 14:46:55 +0700

    menambahkan object DestinasiDetail : DestinasiNavigasi, dan function DetailView

--- app/src/main/java/com/example/pertemuan12_dbeksternal/ui/View/DetailView.kt
+++ app/src/main/java/com/example/pertemuan12_dbeksternal/ui/View/DetailView.kt
@@ -1,22 +1,108 @@
 package com.example.pertemuan12_dbeksternal.ui.View
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12_dbeksternal.model.Mahasiswa
+import com.example.pertemuan12_dbeksternal.ui.CustomWidget.TopAppBar
+import com.example.pertemuan12_dbeksternal.ui.ViewModel.DetailUiState
+import com.example.pertemuan12_dbeksternal.ui.ViewModel.DetailViewModel
+import com.example.pertemuan12_dbeksternal.ui.ViewModel.PenyediaViewModel
+import com.example.pertemuan12_dbeksternal.ui.navigation.DestinasiNavigasi
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "detail mahasiswa"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailView(
+    nim: String,
+    navigateBack: () -> Unit,
+    onClick: () -> Unit,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState by viewModel.detailUiState.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    viewModel.getMahasiswaById(nim)
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMahasiswaById(nim)
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "edit mahasiswa")
+            }
+        },
+    ) { innerPadding ->
+        when (uiState) {
+            is DetailUiState.Loading -> {
+                Text("Loading", Modifier.fillMaxSize())
+            }
+            is DetailUiState.Success -> {
+                val mahasiswa = (uiState as DetailUiState.Success).mahasiswa
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    ItemDetailMhs(mahasiswa = mahasiswa)
+                }
+            }
+            is DetailUiState.Error -> {
+                Text(
+                    text = "gagal memuat data",
+                    modifier = Modifier.fillMaxSize(),
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailMhs(
