fd1a71548c0028628d0749370572e284e3404340
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-30 14:51:36 +0700

    menambahkan class UpdateViewModel, dan function2

--- app/src/main/java/com/example/pertemuan12_dbeksternal/ui/ViewModel/UpdateViewModel.kt
+++ app/src/main/java/com/example/pertemuan12_dbeksternal/ui/ViewModel/UpdateViewModel.kt
@@ -1,6 +1,41 @@
 package com.example.pertemuan12_dbeksternal.ui.ViewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12_dbeksternal.model.Mahasiswa
+import com.example.pertemuan12_dbeksternal.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(UpdateUiState())
+        private set
+    fun loadMahasiswa(nim: String) {
+        viewModelScope.launch {
+            try {
+                val mahasiswa = mhs.getMahasiswaById(nim)
+                uiState = UpdateUiState(updateUiEvent = mahasiswa.toUpdateUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+    fun updateMahasiswaState(updateUiEvent: UpdateUiEvent) {
+        uiState = UpdateUiState(updateUiEvent = updateUiEvent)
+    }
+    suspend fun updateMahasiswa(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.updateMahasiswa(nim, uiState.updateUiEvent.toMahasiswa())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
 
 data class UpdateUiState(
     val updateUiEvent: UpdateUiEvent = UpdateUiEvent()
