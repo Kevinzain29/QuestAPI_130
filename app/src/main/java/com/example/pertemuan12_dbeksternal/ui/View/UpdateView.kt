24a9e69937b6215e74ae41322ce99a3ae1b8ca1e
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-30 14:54:10 +0700

    menambahkan file UpdateView: fun FormInput

---
+++ app/src/main/java/com/example/pertemuan12_dbeksternal/ui/View/UpdateView.kt
@@ -0,0 +1,87 @@
+package com.example.pertemuan12_dbeksternal.ui.View
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan12_dbeksternal.ui.ViewModel.UpdateUiEvent
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    updateUiEvent: UpdateUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (UpdateUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        OutlinedTextField(
+            value = updateUiEvent.nama,
+            onValueChange = { onValueChange(updateUiEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.nim,
+            onValueChange = { onValueChange(updateUiEvent.copy(nim = it)) },
+            label = { Text("NIM") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.alamat,
+            onValueChange = { onValueChange(updateUiEvent.copy(alamat = it)) },
+            label = { Text("Alamat") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.jenisKelamin,
+            onValueChange = { onValueChange(updateUiEvent.copy(jenisKelamin = it)) },
+            label = { Text("Jenis Kelamin") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.kelas,
+            onValueChange = { onValueChange(updateUiEvent.copy(kelas = it)) },
+            label = { Text("Kelas") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.angkatan,
+            onValueChange = { onValueChange(updateUiEvent.copy(angkatan = it)) },
+            label = { Text("Angkatan") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled) {
+            Text(
+                text = "Isi Data",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
