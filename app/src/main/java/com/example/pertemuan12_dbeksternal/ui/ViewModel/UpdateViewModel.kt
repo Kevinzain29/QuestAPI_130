d1f3c53c32152060f70e99ae692c9bc05f8b40ae
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-30 14:49:56 +0700

    menambahkan file UpdateViewModel: data class UpdateUiEvent, UpdateUiState, function toMahasiswa, toUpdateUiEvent

---
+++ app/src/main/java/com/example/pertemuan12_dbeksternal/ui/ViewModel/UpdateViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.pertemuan12_dbeksternal.ui.ViewModel
+
+import com.example.pertemuan12_dbeksternal.model.Mahasiswa
+
+data class UpdateUiState(
+    val updateUiEvent: UpdateUiEvent = UpdateUiEvent()
+)
+
+data class UpdateUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun UpdateUiEvent.toMahasiswa(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUpdateUiEvent(): UpdateUiEvent = UpdateUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
