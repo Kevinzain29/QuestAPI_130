80b74e4b72e8aac5a5ed743fe9c2cffb1cbd42b9
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2024-12-30 15:08:55 +0700

    update MainActivity, MahasiswaApp

--- app/src/main/java/com/example/pertemuan12_dbeksternal/ui/ViewModel/MahasiswaApp.kt
+++ app/src/main/java/com/example/pertemuan12_dbeksternal/ui/ViewModel/MahasiswaApp.kt
@@ -13,7 +13,7 @@ import com.example.pertemuan12_dbeksternal.ui.navigation.PengelolaHalaman
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MahasiswaApp() {
+fun MahasiswaApp(modifier: Modifier) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = Modifier
